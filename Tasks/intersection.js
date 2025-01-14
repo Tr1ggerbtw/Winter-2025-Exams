398b22bd3bda0584e05c0b649c6b612b509ecdb6
Tr1ggerbtw <lehesadanylo27@gmail.com>
2025-01-14 14:30:48 +0200

    Update intersection.js
    Removed unnecessary {} blocks & add const

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -4,23 +4,13 @@
 'use strict';
 
 function intersection(firstDict, secondDict) {
-  first_keys = Object.keys(firstDict);
-  {
-  }
-  for (attribute_name of first_keys) {
+  const first_keys = Object.keys(firstDict);
+  for (const attribute_name of first_keys) {
     if (firstDict[attribute_name] === secondDict[attribute_name]) {
       secondDict[attribute_name] = firstDict[attribute_name];
-      {
-      }
     } else {
       delete firstDict[attribute_name];
-      {
-      }
     }
-    {
-    }
-  }
-  {
   }
   return firstDict;
 }
