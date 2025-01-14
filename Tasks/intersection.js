171eba3270ccf6b620664b8549508c8d4a7d666d
Tr1ggerbtw <lehesadanylo27@gmail.com>
2025-01-14 14:37:31 +0200

    Update intersection.js
    Added result to simplify and prevent modifying origin. dictionaries

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -4,12 +4,11 @@
 'use strict';
 
 function intersection(firstDict, secondDict) {
+  const result = {};
   const first_keys = Object.keys(firstDict);
   for (const attribute_name of first_keys) {
     if (firstDict[attribute_name] === secondDict[attribute_name]) {
       secondDict[attribute_name] = firstDict[attribute_name];
-    } else {
-      delete firstDict[attribute_name];
     }
   }
   return firstDict;
