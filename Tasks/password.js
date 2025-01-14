ad926e567a144bd60ffc4571a5c5ae4597da7e01
Tr1ggerbtw <lehesadanylo27@gmail.com>
2025-01-14 17:59:03 +0200

    Update password.js
    changing identifier to camelCase & use strict & alphabet to char, because in our set of characters could be numbers

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,14 +1,15 @@
 // Refactor following solution
 // Generate random password
 
-let GeneratePassword = (alphabet, length) => {
-  const MAX = alphabet.length;
+'use strict';
+const generatePassword = (char, length) => {
+  const MAX = char.length;
   key = '';
   for (let i = 0; i < length; i++) {
     Index = Math.floor(Math.random() * MAX);
-    key = key + alphabet[Index];
+    key = key + char[Index];
   }
   return key;
 };
 
-module.exports = GeneratePassword;
+module.exports = generatePassword;
